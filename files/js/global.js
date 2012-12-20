727841389190404b0e57badaae7989119850a99b
Master <github@agentur-vida.de>
2012-12-20 15:00:34 +0100

    CSS and JS added

---
+++ files/js/global.js
@@ -0,0 +1,14 @@
+/**
+ * Global Javascripts
+ * author Maik. H
+ * <www.agentur-vida.de>
+ */
+
+// Section document-ready
+$(document).ready(function()
+{
+  
+});
+
+// functions
+// Some changes to commit
